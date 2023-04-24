a8690a785e3cd46498de7886507856fc8e1ec612
Glenn Phillips <gjp214@gmail.com>
2023-04-24 15:23:14 -0400

    test

--- FrontEnd/QueryBuilder.py
+++ FrontEnd/QueryBuilder.py
@@ -1,5 +1,6 @@
 from DB import DBSelect
 from DB import DBExtract
+from pprint import pprint
 
 
 """
@@ -13,10 +14,17 @@ from DB import DBExtract
         Glenn Phillips 	443-915-0172
 """
 
-class TableInfo:
+class QueryBuilder:
+    def __init__(self, db_name: str):
+        self.extract = DBExtract.DBExtract(db_name)
+        self.select = DBSelect.DBSelect(db_name)
+
+
+class TableInfo(QueryBuilder):
     def __init__(self, db_name: str, table_name: str):
-        self._extract = DBExtract.DBExtract(db_name)
-        self._select = DBSelect.DBSelect(db_name)
+        super().__init__(db_name)
+        self._extract = self.extract
+        self._select = self.select
         self._table_name = table_name
         self._table_pk_headers = self._extract.get_table_pk_headers_from_table(table_name)
         self._table_fk_headers = self._extract.get_table_fk_headers_from_table(table_name)
@@ -41,62 +49,45 @@ class TableInfo:
 
     @property
     def all_headers(self):
-        return self._table_pk_headers + self._table_fk_headers + self._table_fk_headers
-
-    def get_from_table_records_id_name_header(self):
-        return_result = []
-        table_name = self.table_name
-        pk_header = self.pk_headers[0]
-        name_field = self.fk_headers
-
-        for col in self._table_data_headers:
-            if 'Name' in col['data_header'] or 'Title' in col['data_header']:
-                return_result.append({'table_name': table_name,
-                                      'id_field': pk_header,
-                                      'name_field': col['data_header']})
-            else:
-                name_field = self.get_table_id_all_cols_as_name(table_name)
-                return_result.append({'table_name': table_name,
-                                      'id_field': pk_header,
-                                      'name_field': name_field})
-                # print(return_result)
-        return return_result
+        return_value = []
+        header_list = [self.pk_headers, self.fk_headers,self.data_headers]
+        for headers in header_list:
+            for item in headers:
+                return_value.append(item)
+        return return_value
 
     def get_table_id_all_cols_as_name(self, table_name):
         query_part_headers = ''
         table_headers = self.all_headers
         for header in table_headers:
-            query_part_headers += f'''{header} || ', ' || '''
+            query_part_headers += f'''{header} || '''
 
-        query_part_headers = query_part_headers[:-11]
+        query_part_headers = query_part_headers[:-3]
 
         query_part = f''' {query_part_headers} as name '''
         #self._log_it(query_part)
         return query_part
 
-    def _switch(self, table_name):
-        if table_name == None:
-            return ""
-        # elif table == "VIEW_GUIManufacturers":
-        #     return super().VIEW_GUIManufacturers()
-        # elif table == "VIEW_GUICustomers":
-        #     return super().VIEW_GUICustomers()
-        elif type(table_name) is dict():
-            query_part = self._extract.get_table_id_name_header(table_name['table_name'])
-            query = '''SELECT {} FROM {}'''.format(table_name['name_field'], table_name['table_name'])
-        else:
-            query_part = self._extract.get_table_id_name_header(table_name)
-            query = '''SELECT {} FROM {}'''.format(query_part['name_field'], query_part['table_name'])
+    def _get_query_select_pk__headers_as_name(self):
+        table_name = self.table_name
+        table_id = self.pk_headers[0]
+        headers = self.get_table_id_all_cols_as_name(table_name)
+        query = '''SELECT {}, {} FROM {}'''.format(table_id, headers, table_name)
+        print(query)
+        return query
 
-    # def _table_data_headers(self):
-    #     pass
-
+    def get_items_pk_headers(self) -> tuple:
+        return_result = self.extract.execute_query(self._get_query_select_pk__headers_as_name())
+        return return_result
 
 
 
 if __name__ == '__main__':
-    app = TableInfo('../DB/databases/test_db3.db', 'Invoices')
+    app = TableInfo('../DB/databases/test_db3.db', 'Customers')
     print(app.pk_headers)
     print(app.fk_headers)
     print(app.data_headers)
-    print(app.get_from_table_records_id_name_header())
+    print(app.all_headers)
+    print(app.get_items_pk_headers())
+
+
