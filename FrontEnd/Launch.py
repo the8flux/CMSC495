4f35d9a1e4006bd9345308305ff42ac4ea449714
Glenn Phillips <gjp214@gmail.com>
2023-05-04 11:08:09 -0400

    Revert "Introducing Wills New font end"
    This reverts commit 009179b56ac086717b6be82b23187064542adb09.

--- FrontEnd/Launch.py
+++
@@ -1,8 +0,0 @@
-from FrontEnd import WebApp
-
-class Launch:
-    def __init__(self):
-        self.running_app = WebApp.WebApp()
-
-    def launch(self):
-        self.running_app.app.run(host='0.0.0.0', port=8080)
--- FrontEnd/main.py
+++
@@ -1,7 +0,0 @@
-from FrontEnd import Launch
-
-if __name__ == '__main__':
-    app = Launch.Launch()
-    app.launch()
-
-
